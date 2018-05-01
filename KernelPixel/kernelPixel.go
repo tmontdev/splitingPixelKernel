f11d63b2fb8a96e65dfce69de816ec198d64045c
jzes <pauloneto.jose@gmail.com>
2018-04-30 22:50:49 -0300

    Its harder than i thought

--- KernelPixel/kernelPixel.go
+++ KernelPixel/kernelPixel.go
@@ -1,53 +1,67 @@
 package KernelPixel
 
 import (
-        "image"
-        "image/png"
-        "image/color"
-        "os"
-        "fmt"
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"os"
 )
 
-type Point struct {
-        x int
-        y int
+type pixel struct {
+	x int
+	y int
 }
 
 func checkErro(erro error) {
-        if erro != nil {
-                panic(erro)
-        }
+	if erro != nil {
+		panic(erro)
+	}
 }
 
 func GetImageData(imagePath string) image.Image {
-        imageFile, erro := os.Open(imagePath)
-        checkErro(erro)
-        defer imageFile.Close()
+	imageFile, erro := os.Open(imagePath)
+	checkErro(erro)
+	defer imageFile.Close()
 
-        imageData, erro := png.Decode(imageFile)
-        checkErro(erro)
+	imageData, erro := png.Decode(imageFile)
+	checkErro(erro)
 
-        return imageData
+	return imageData
 }
 
-func GetSimilarityOfDataImages(imageFull image.Image, imagePartial image.Image){
-        
-        for x := 0; x <= imageFull.Bounds().Max.X; x++ {
-                for y := 0; y <=imageFull.Bounds().Max.Y; y++ {
-                        fmt.Println(searchPixel(imageFull.At(x, y), imagePartial))
-                }
-        }
+func isPixielInPixels(pix pixel, pixels []pixel) bool {
+	for _, pixelTime := range pixels {
+		if pixelTime == pix {
+			return true
+		}
+	}
+	return false
 }
 
+func GetSimilarityOfDataImages(imageFull image.Image, imagePartial image.Image) {
+	pixels := make([]pixel, 1)
+	for x := 0; x <= imageFull.Bounds().Max.X; x++ {
+		for y := 0; y <= imageFull.Bounds().Max.Y; y++ {
+			if searchPixel(imageFull.At(x, y), imagePartial) {
+				if isPixielInPixels(pixel{x, y - 1}, pixels) {
+					pixels = append(pixels, pixel{x, y})
+				}
 
-func searchPixel(pixel color.Color, imageToCompare image.Image) int{
-        var equalPixels int
-        for x := 0; x <= imageToCompare.Bounds().Max.X; x++ {
-                for y := 0; y <=imageToCompare.Bounds().Max.Y; y++ {
-                        if imageToCompare.At(x, y) == pixel {
-                                equalPixels++
-                        }
-                }
-        }
-        return equalPixels
+			}
+		}
+	}
+	fmt.Println(pixels)
+	fmt.Println(len(pixels))
+}
+
+func searchPixel(pixel color.Color, imageToCompare image.Image) bool {
+	for x := 0; x <= imageToCompare.Bounds().Max.X; x++ {
+		for y := 0; y <= imageToCompare.Bounds().Max.Y; y++ {
+			if imageToCompare.At(x, y) == pixel {
+				return true
+			}
+		}
+	}
+	return false
 }
--- main.go
+++ main.go
@@ -5,11 +5,11 @@ import "splitingPixelKernel/KernelPixel"
 //import "os"
 
 func main() {
-        //imagePath := os.Args[1]
-        imagePathFull := "/home/cabox/workspace/src/splitingPixelKernel/testFiles/full.png"
-        imagePathPartial := "/home/cabox/workspace/src/splitingPixelKernel/testFiles/partial.png"
-        imageDataFull := KernelPixel.GetImageData(imagePathFull)
-        imageDataPartial := KernelPixel.GetImageData(imagePathPartial)
-        KernelPixel.GetSimilarityOfDataImages(imageDataFull, imageDataPartial)
-        
+	//imagePath := os.Args[1]
+	imagePathFull := "/home/jzes/lab/go/src/splitingPixelKernel/testFiles/full.png"
+	imagePathPartial := "/home/jzes/lab/go/src/splitingPixelKernel/testFiles/partial.png"
+	imageDataFull := KernelPixel.GetImageData(imagePathFull)
+	imageDataPartial := KernelPixel.GetImageData(imagePathPartial)
+	KernelPixel.GetSimilarityOfDataImages(imageDataFull, imageDataPartial)
+
 }
